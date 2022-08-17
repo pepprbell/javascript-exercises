06d7cf461f6d1f9623e1585b39fb0ed192b77d3a
pepprbell <pepprbell@gmail.com>
2022-08-17 18:40:49 +0900

    Pass exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let res = ''
+    for (let i = 0; i < num; i++) {
+        res += string        
+    }
+    return res
 };
 
 // Do not edit below this line
