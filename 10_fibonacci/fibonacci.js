120f2546efd0155e6be848f319aa0d088360cd41
pepprbell <pepprbell@gmail.com>
2022-08-21 17:31:20 +0900

    Pass exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n<0) {
+        return 'OOPS'
+    }
+    let res = [0,1,1]
+    for (let i = 2; i < n; i++) {
+        res.push(res[i-1]+res[i])        
+    }
+    return res[n]
 };
 
 // Do not edit below this line
