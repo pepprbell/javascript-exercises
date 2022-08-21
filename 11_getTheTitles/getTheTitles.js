0c785d90325caa9ddf5974d0e203816cdc5effd2
pepprbell <pepprbell@gmail.com>
2022-08-21 17:50:20 +0900

    Pass exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(data) {
+    let res = []
+    data.reduce((prev, curr) => res.push(curr.title) , [])
+    return res
 };
 
 // Do not edit below this line
