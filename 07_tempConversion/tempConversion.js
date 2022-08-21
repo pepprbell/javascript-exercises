a20fb9f61ce0359026313e3139347ce17c7bf409
pepprbell <pepprbell@gmail.com>
2022-08-21 15:39:59 +0900

    Pass exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fer) {
+  return Math.round((fer - 32) * 50/9)/10
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  return Math.round(cel * 90/5 + 320)/10
 };
 
 // Do not edit below this line
