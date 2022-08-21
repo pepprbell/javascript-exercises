8e355393f14737271599ec09bf0e2ff2912dc1f0
pepprbell <pepprbell@gmail.com>
2022-08-21 14:24:17 +0900

    Pass exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, num) {
+    let i = arr.indexOf(num)
+    arr.splice(i,1)
+    return arr
 };
 
 // Do not edit below this line
