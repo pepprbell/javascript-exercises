f1321733fb00388fde565016b6d38cd7547dd29c
pepprbell <pepprbell@gmail.com>
2022-08-21 17:23:22 +0900

    Pass exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[^a-z]/gi, '').toUpperCase()
+    for (let i = 0; i < Math.floor(word.length/2); i++) {
+        if (word[i] !== word[word.length-i-1]) {
+            return false
+        }
+    }
+    return true
 };
-
 // Do not edit below this line
 module.exports = palindromes;
