38892a4eeb9c05afeefb32a9a5d0d734b77eadce
pepprbell <pepprbell@gmail.com>
2022-08-21 14:25:51 +0900

    Pass exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let res = 0
+    for (let i = start; i < end+1; i++) {
+        res += i
+    }
+    return res
 };
 
 // Do not edit below this line
