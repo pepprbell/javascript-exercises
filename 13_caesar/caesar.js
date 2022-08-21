d80c4bbb3488a402206a08a7772a26f90aa92c77
pepprbell <pepprbell@gmail.com>
2022-08-21 21:32:33 +0900

    Pass exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,22 @@
-const caesar = function() {
+function btwn(i,x) {
+    let I = i.toUpperCase()
+    if (i === I) {
+        return (65 <= x && x <= 90)
+    }
+    return (97 <= x && x <= 122)
+}
 
+const caesar = function(word, offset) {
+    let res = ''
+    offset = offset%26
+    for (let i = 0; i < word.length; i++) {
+        let x = word.charCodeAt(i)
+        if (btwn(word[i],x)) {
+            x += btwn(word[i],x+offset) ? offset : Math.sign(offset)*(-26)+offset
+        }
+        res += String.fromCharCode(x)
+    }
+    return res
 };
 
 // Do not edit below this line
