43c88ffa0409b78a366e39fae30d49f1f034ba05
pepprbell <pepprbell@gmail.com>
2022-08-21 16:53:19 +0900

    Solve additional testcase

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,10 @@
 const sumAll = function(start, end) {
+    if (end < start) {
+        [start, end] = [end, start]
+    }
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start*end < 0) {
+        return 'ERROR'
+    }
     let res = 0
     for (let i = start; i < end+1; i++) {
         res += i
