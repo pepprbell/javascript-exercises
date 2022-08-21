06b36cf651afb77651e8f572201c7938675743bc
pepprbell <pepprbell@gmail.com>
2022-08-21 14:16:15 +0900

    Pass exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let res = ''
+    const arr = str.split('')
+    for (let i = 1; i < arr.length+1; i++) {
+        res += arr[arr.length-i]
+    }
+    return res
 };
 
 // Do not edit below this line
