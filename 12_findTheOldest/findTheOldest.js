6602171b23a157300bf32f21088636b52e6dc79b
pepprbell <pepprbell@gmail.com>
2022-08-21 18:10:13 +0900

    Pass exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(data) {
+    const now = new Date().getFullYear()
+    return data.reduce((prev, curr) =>
+    (curr.yearOfDeath ? curr.yearOfDeath : now)-curr.yearOfBirth > (prev.yearOfDeath ? prev.yearOfDeath : now)-prev.yearOfBirth ? prev=curr : prev , data[0])
 };
 
 // Do not edit below this line
